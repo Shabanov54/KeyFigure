ea58bfcf323252858e1eef6ff983a57770063a4c
Shabanov54 <67888425+Shabanov54@users.noreply.github.com>
2020-07-07 12:31:32 +0700

    Add files via upload

---
+++ key-figure/Contex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace key_figure
+{
+    public class Contex
+    {
+        long Codes  = 0;
+        internal void InputCode(string addtxt)
+        {
+            try
+            {
+                if (addtxt.Length != 12)
+                {
+                    throw new Exception();
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Выедено меньше или больше 12 симолов");
+                throw new Exception();
+            }
+            finally
+            {
+                var code = Convert.ToInt64(addtxt);
+                Codes = code;
+            }
+        }
+        internal long GetCodes()
+        {
+            return Codes;
+        }
+
+    }
+}
---
+++ key-figure/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace key_figure
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Contex contex;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contex = new Contex();
+            
+        }
+        private void Button_add_Click(object sender, RoutedEventArgs e)
+        {
+            contex.InputCode(text.Text);
+            var codes = contex.GetCodes();
+            var operation = new Operation();
+            operation.Run(codes);
+            var barcode = operation.GetBarcode();
+            var output = new OutPut();
+            output.Display_Barcode(codes,barcode, RadyCode);
+        }
+
+    }
+}
